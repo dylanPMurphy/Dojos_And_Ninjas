2d2d7e496fab61414dff7379872318cfbcf768b7
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-03 12:55:14 -0700

    need to fix dojo seletion for ninjas

---
+++ dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from dojo_ninjas_app.models import *
+
+# Create your views here.
+def index(request):
+    context = {
+        'dojos': Dojo.objects.all(),
+        'ninjas': Ninja.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def createDojo(request):
+    Dojo.objects.create(
+        name = request.POST['name'],
+        city = request.POST['city'],
+        state = request.POST['state']
+    )
+    return redirect('/')
+
+def createNinja(request):
+    Ninja.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        my_dojo = request.POST['my_dojo']
+    )
+    return redirect('/')
